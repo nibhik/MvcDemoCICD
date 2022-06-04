a6be72390ef5aa17881039d66c8f24eed360e25d
nibhik <nibhik.kokate@cognizant.com>
2022-06-04 15:12:32 +0530

    Add project files.

---
+++ MvcDemoCICD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcDemoCICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
